399bbb843628febdca33354abf4d3cebafa6e0c6
xdKya <90745676+xdKya@users.noreply.github.com>
2023-05-10 19:10:45 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# site
+# Portfolio-
---
+++ seila.js
@@ -0,0 +1,28 @@
+
+
+
+
+function draw(){
+    batatas()
+}
+
+function batatas(){
+    // resutado
+    repolho()
+}
+
+
+function repolho(){
+    // repolhos
+}
+
+
+
+
+function draw(){
+    ()=>{
+    // resutado 
+    repolho()
+    }
+}
+
